c0a786538d528ee5a27305be63d222e36937306e
Allan Roque <allanrockstifo@hotmail.com>
2019-08-17 21:12:00 -0300

    deu certo

--- learning.py
+++ learning.py
@@ -3,9 +3,26 @@ from luis_api import *
 filesPath = ["Audios/transcript/capex.txt", "Audios/transcript/gog.txt", "Audios/transcript/marcos_criticos.txt", "Audios/transcript/obrigacao.txt"]
 types = ["capex", "gog", "marcos_criticos", "obrigacao"]
 
-for idx in range(0,3):
+matrix = []
+listinha = []
+
+for idx in range(0,4):
+    matrix = []
+    listinha = []
     file = open(filesPath[idx], 'r')
     for line in file:
-        print(postMsg(line, types[idx]))
+        if len(listinha) == 50:
+            matrix.append(listinha)
+            listinha = []
+        listinha.append({
+            "text": line,
+            "intentName": types[idx]
+        })
+    if len(listinha) != 0:
+        matrix.append(listinha)
+    for coisinha in matrix:
+        print(postMsg(coisinha))
+    
     print("Lista {} Concluida".format(idx))
     file.close()
+    matrix = []
